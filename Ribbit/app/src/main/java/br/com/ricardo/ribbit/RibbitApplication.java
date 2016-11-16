b97924fb2c9138ac481066fcebc435c1b7146de2
Ricardo Teixeira <ricardo_teixeira@hotmail.com.br>
2016-11-15 23:50:13 -0200

    Initializing Parse.com

---
+++ Ribbit/app/src/main/java/br/com/ricardo/ribbit/RibbitApplication.java
@@ -0,0 +1,26 @@
+package br.com.ricardo.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by Ricardo on 15/11/2016.
+ */
+
+public class RibbitApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("YOUR_APP_ID")
+                .server("http://YOUR_PARSE_SERVER:1337/parse")
+                .build()
+        );
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+}
